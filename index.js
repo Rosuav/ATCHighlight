02ac8de47f3e3f6f8f1e57dbb50391aa01eb0672
Chris Angelico <rosuav@gmail.com>
2021-08-11 15:42:01 +1000

    Actually show the images

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import {on, fix_dialogs} from "https://rosuav.github.io/shed/chocfactory.js";
 import "https://cdn.jsdelivr.net/npm/comfy.js/dist/comfy.min.js"; const ComfyJS = window.ComfyJS;
-const {LI, B} = choc;
+const {P, IMG} = choc;
+let participantdata = null;
+
+fetch("https://sikorsky.rosuav.com/static/atc-" + window.location.hash.slice(1) + ".json")
+	.then(r => r.json())
+	.then(data => participantdata = data)
+	.catch(e => set_content("main", P("Can't load JSON file, check ID")));
 
 ComfyJS.onChat = ( user, message, flags, self, extra ) => {
-	DOM("#info").appendChild(LI([B(user), ": " + message]));
+	if (!participantdata) return;
+	if (user !== "Rosuav") return;
+	const m = /maayaBrush ([^ ]+) is one of our #100Artists/.exec(message);
+	if (!m) return;
+	const artist = m[1], link = participantdata.images[m[1]];
+	if (!link) return;
+	set_content("main", [
+		P("ATC from " + artist),
+		IMG({src: link}),
+	]).classList.add("show");
+	setTimeout(() => DOM("main").classList.remove("show"), 10000);
 }
 ComfyJS.Init("mrsoef5");
